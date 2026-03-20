dc8221bb697347bee9c506c486ff5b71811a86d1
Nico <nazucca7@gmail.com>
2026-03-20 10:20:59 -0600

    Added basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![no_std]
 
-// Defines a mk_static macro.
-// Macro is used to reserve memory at compile time
-// https://docs.rs/static_cell/latest/static_cell/#:~:text=reserve%20memory%20at%20compile%20time%20for%20a%20value
-// This can be done with Box, unsafe code, or std in less limited environments
+/// Defines a mk_static macro.
+/// Macro is used to reserve memory at compile time
+/// <https://docs.rs/static_cell/latest/static_cell/#:~:text=reserve%20memory%20at%20compile%20time%20for%20a%20value>
+/// This can be done with Box, unsafe code, or std in less limited environments
 #[macro_export]
 macro_rules! mk_static {
     ($t:ty,$val:expr) => {{
@@ -14,8 +14,11 @@ macro_rules! mk_static {
     }};
 }
 
+/// Used to configure HID strings, to allow your device to be easily identified.
 pub mod hid_config;
 pub(crate) mod hid_report_writer;
+/// Object used to send HID keyboard reports.
 pub mod keyboard;
 pub(crate) mod keyboard_report;
+/// HID compatible keycodes to be used with keyboard object.
 pub mod keycodes;
